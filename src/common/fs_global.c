29c02d4dc333cb5515e951be91bceda3d9c98079
YuQing <384681@qq.com>
2021-02-24 11:22:15 +0800

    upgrade version to 1.3.0

--- src/common/fs_global.c
+++ src/common/fs_global.c
@@ -16,7 +16,7 @@
 #include "fs_global.h"
 
 FSGlobalVars g_fs_global_vars = {
-    {1, 2, 1},
+    {1, 3, 0},
     {FS_DATA_GROUP_MASTER_HC_SEED0,
         FS_DATA_GROUP_MASTER_HC_SEED1,
         FS_DATA_GROUP_MASTER_HC_SEED2}
